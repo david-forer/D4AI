342ed064d052136833dcd43f5ff98c05550eb2c1
David J Forer <david.j.forer@gmail.com>
2024-11-03 22:11:10 +0000

    added pages and components

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Include Astro and JavaScript/TypeScript files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
