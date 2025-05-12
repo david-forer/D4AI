06574252552c86c93314a7563fd1f85715adb22d
David J Forer <david.j.forer@gmail.com>
2025-05-13 00:31:39 +0100

    Remove storyblok from astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [
     tailwind(),
-    storyblok({
-      accessToken: 'YOUR_PREVIEW_TOKEN',
-      components: {
-        blogPost: 'components/BlogPost',
-      },
-    }),
+    
   ],
 });
